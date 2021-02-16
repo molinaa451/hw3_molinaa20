4cf86e507aadd5b3649baaef6d47280e2ae84cea
A Molina <70313285+molinaa451@users.noreply.github.com>
2021-02-15 21:15:43 -0700

    Spec 5

--- hw3/public/javascripts/changeName.js
+++ hw3/public/javascripts/changeName.js
@@ -1,4 +1,5 @@
-/* Drop Down Item: name Change source: https://stackoverflow.com/questions/54437917/replace-the-heading-name-with-dropdown-name-when-clicked/54438314#54438314*/
+/* Drop Down Item: name change
+source: https://stackoverflow.com/questions/54437917/replace-the-heading-name-with-dropdown-name-when-clicked/54438314#54438314*/
 eventHandler =function(event) {
     $('.dropdown-item').click(function() {
         $('.dropbtn').html($(this).html());
